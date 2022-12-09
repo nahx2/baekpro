bfcffe7d7c65dd139ebd0f9a392011e4d18d2e6d
nahx2 <108976269+nahx2@users.noreply.github.com>
2022-12-09 15:46:13 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+       
+        int answer = 0;
+       while(n!=0){
+           answer += n%10;
+           n = n/10;
+       }
+        System.out.println(answer);
+
+        return answer;
+    }
+}
