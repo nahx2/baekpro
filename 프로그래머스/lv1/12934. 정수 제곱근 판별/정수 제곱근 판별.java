9b6a8f198d978587350bb2dbf23d9b2cf317e27e
nahx2 <108976269+nahx2@users.noreply.github.com>
2022-12-13 23:09:38 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,10 +1,12 @@
 class Solution {
     public long solution(long n) {
+        long a = -1;
         for(long x=0;x<=n;x++){
             if(n==x*x){
-                return (x+1)*(x+1);
+                a = (x+1)*(x+1);
+                return a;
             }
         }
-       return -1;
+       return a;
     }
 }
