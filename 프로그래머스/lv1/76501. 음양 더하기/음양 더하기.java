64b8d8d0dc4f2a656c599e9355281717b73b110f
nahx2 <108976269+nahx2@users.noreply.github.com>
2023-03-05 00:25:49 +0900

    [level 1] Title: 음양 더하기, Time: 6.57 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<signs.length;i++){
+            System.out.println(signs[i]);
+            if(signs[i] == false){
+                answer += (absolutes[i]*-1);
+            }else{
+                answer += absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
