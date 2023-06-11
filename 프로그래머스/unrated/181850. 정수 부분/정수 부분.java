e77cf3c6345b2b08742e7f004641858df6323b99
nahx2 <108976269+nahx2@users.noreply.github.com>
2023-06-11 09:53:40 +0900

    [unrated] Title: 정수 부분, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(double flo) {
+        //int answer = 0;
+        // flo보다 i가 크면
+        // answer에 값 담기
+        //for(int i=0;i<101;i++){
+        //        while(flo<i){
+        //            answer = i-1;
+        //        }
+        //}
+        int answer = (int) flo;
+        
+        return answer;
+    }
+}
