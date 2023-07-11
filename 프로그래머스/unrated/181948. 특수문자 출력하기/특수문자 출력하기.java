80cd695c111cbfb2aada4993bf04a79aa13571af
nahx2 <108976269+nahx2@users.noreply.github.com>
2023-07-11 09:45:56 +0900

    [unrated] Title: 특수문자 출력하기, Time: 178.28 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // \백슬래시 앞에 \백슬래시를 붙여줘야 출력되고 "이것도 \백슬래시를 붙여줘야 출ㄹ력됨
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
