9baaefe4a02fea8b5ffc408cb6767eb0f114623f
nahx2 <108976269+nahx2@users.noreply.github.com>
2023-07-13 12:33:39 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.19 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+String ab = String.valueOf(a)+String.valueOf(b);
+        String ba = String.valueOf(b)+String.valueOf(a);
+int iab = Integer.valueOf(ab);
+        int iba = Integer.valueOf(ba);
+if(iab>iba){
+    answer=iab;
+}else if(iba>iab){
+    answer=iba;
+}else if(iab==iba){
+    answer=iab;
+}
+        return answer;
+    }
+}
